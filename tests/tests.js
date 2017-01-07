97292f5c03eed295175979090f45e242a6a8b307
Akinbode Femi <femi661@gmail.com>
2017-01-07 13:09:54 +0100

    Updated to jasmine sytle

--- tests/tests.js
+++ tests/tests.js
@@ -1,52 +1,51 @@
-// testing code goes here
+(function(){
+  'use strict';
 
-'use strict'
-
-var chai = require('chai');
-var expect = chai.expect;
-
-var myApp = require('../app/fizzBuzz.js');
-
-describe("Fizz Buzz tests ", function() {
+  var jasmine = require('jasmine');
+  var myApp = require('../app/fizzBuzz.js');
+ 
+  
+  describe("Fizz Buzz tests ", function() {
 
   it("should return `Fizz` for number divisible by 3", function() {
-    expect(myApp.fizzBuzz(3)).to.be.equal('Fizz');
+    expect(myApp.fizzBuzz(3)).toBe('Fizz');
   });
 
   it("should return `Buzz` for number divisible by 5", function() {
-    expect(myApp.fizzBuzz(5)).to.be.equal('Buzz');
+    expect(myApp.fizzBuzz(5)).toBe('Buzz');
   });
 
   it("should return `FizzBuzz` for 15", function() {
-    expect(myApp.fizzBuzz(15)).to.be.equal('FizzBuzz');
+    expect(myApp.fizzBuzz(15)).toBe('FizzBuzz');
   });
 
   it("should return `FizzBuzz` for 45", function() {
-    expect(myApp.fizzBuzz(45)).to.be.equal('FizzBuzz');
+    expect(myApp.fizzBuzz(45)).toBe('FizzBuzz');
   });
 
   it("should return `FizzBuzz` for 90", function() {
-    expect(myApp.fizzBuzz(90)).to.be.equal('FizzBuzz');
+    expect(myApp.fizzBuzz(90)).toBe('FizzBuzz');
   });
 
   it("should return `Fizz` for 63", function() {
-    expect(myApp.fizzBuzz(63)).to.be.equal('Fizz');
+    expect(myApp.fizzBuzz(63)).toBe('Fizz');
   });
 
   it("should return 7 since its indivisible by 3 and 5", function() {
-    expect(myApp.fizzBuzz(7)).to.be.equal(7);
+    expect(myApp.fizzBuzz(7)).toBe(7);
   });
 
   it("should return 101 since its indivisible by 3 and 5", function() {
-    expect(myApp.fizzBuzz(101)).to.be.equal(101);
+    expect(myApp.fizzBuzz(101)).toBe(101);
   });
 
   it("should return 101 since its indivisible by 3 and 5", function() {
-    expect(myApp.fizzBuzz('11o')).to.be.equal('Invalid Argument, Enter A Number Next Time');
+    expect(myApp.fizzBuzz('11o')).toBe('Invalid Argument, Enter A Number Next Time');
   });
 
   it("should return 101 since its indivisible by 3 and 5", function() {
-    expect(myApp.fizzBuzz([67])).to.be.equal('Invalid Argument, Enter A Number Next Time');
+    expect(myApp.fizzBuzz([67])).toBe('Invalid Argument, Enter A Number Next Time');
   });
 
-});
+  });
+})();
