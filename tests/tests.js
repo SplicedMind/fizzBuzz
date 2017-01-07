ab09e451f0a4be08c7f0791b035914b0fee261a9
Akinbode Femi <femi661@gmail.com>
2017-01-07 08:19:38 +0100

    Corrected the `expect` assertion syntax

--- tests/tests.js
+++ tests/tests.js
@@ -3,51 +3,42 @@
 'use strict'
 
 var chai = require('chai');
-var assert = chai.assert;
 var expect = chai.expect;
-var should = chai.should();
 
-var myApp = require('../app/library.js');
+var myApp = require('../app/fizzBuzz.js');
 
-describe("Test for proper arithmetic functionality", function() {
-  it("should return 2 as average for 1, 2, 3", function() {
-    assert(myApp.computeAverage(1,2,3) == 2);
-    expect(myApp.computeAverage(1,2,3)).to.equal(2);
-    (myApp.computeAverage(1,2,3)).should.equal(2);
-  })
-  it("should return 5 as average for 3, 7, 5", function() {
-    assert(myApp.computeAverage(3, 7, 5) == 5);
-    expect(myApp.computeAverage(3, 7, 5)).to.equal(5);
-    (myApp.computeAverage(3, 7, 5)).should.equal(5);
-  })
-  it("should return 120 as factorial for 5", function() {
-    assert(myApp.computeFactorial(5) == 120);
-    expect(myApp.computeFactorial(5)).to.equal(120);
-    (myApp.computeFactorial(5)).s.equal(120);
-  })
-})
+describe("Fizz Buzz tests ", function() {
 
+  it("should return `Fizz` for number divisible by 3", function() {
+    expect(fizzBuzz(3)).to.be('Fizz');
+  });
 
-describe("Test for temperature conversion functionality", function() {
-  it("should return X for Celcius value Y", function() {
-    assert(myApp.convertTempCtoF(40) == 104);
-    expect(myApp.convertTempCtoF(40)).to.equal(104);
-    (myApp.convertTempCtoF(40)).should.equal(104);
-  })
-  it("should return X for Celcius value Y", function() {
-    assert(myApp.convertTempCtoF(60) == 140);
-    expect(myApp.convertTempCtoF(60)).to.equal(140);
-    (myApp.convertTempCtoF(60)).should.equal(140);
+  it("should return `Buzz` for number divisible by 5", function() {
+    expect(fizzBuzz(5)).to.be('Buzz');
+  });
 
-  })
-  it("should return Y for Fahrenheit value X", function() {
-    assert(myApp.convertTempFtoC(140) == 60);
-    expect(myApp.convertTempFtoC(140)).to.equal(60);
-    (myApp.convertTempFtoC(140)).should.equal(60);
-  })
-  it("should return Y for Fahrenheit value X", function() {
-    assert(myApp.convertTempFtoC(104) == 40);
-    expect(myApp.convertTempFtoC(104)).to.equal(40);
-    (myApp.convertTempFtoC(104)).should.equal(40);
-  })
-})
+  it("should return `FizzBuzz` for 15", function() {
+    expect(fizzBuzz(15)).to.be('FizzBuzz');
+  });
+
+  it("should return `FizzBuzz` for 45", function() {
+    expect(fizzBuzz(45)).to.be('FizzBuzz');
+  });
+
+  it("should return `FizzBuzz` for 90", function() {
+    expect(fizzBuzz(90)).to.be('FizzBuzz');
+  });
+
+  it("should return `Fizz` for 63", function() {
+    expect(fizzBuzz(63)).to.be('Fizz');
+  });
+
+  it("should return 7 since its indivisible by 3 and 5", function() {
+    expect(fizzBuzz(7)).to.be(7);
+  });
+
+  it("should return 101 since its indivisible by 3 and 5", function() {
+    expect(fizzBuzz(101)).to.be(101);
+  });
+
+});
